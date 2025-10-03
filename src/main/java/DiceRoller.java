67adad3aebdb487e811d620e18b3e0f3a19a711a
Anderscodes1 <anderschasegustafson@icloud.com>
2025-10-03 13:49:35 -0400

    Update DiceRoller.java

--- src/main/java/DiceRoller.java
+++ src/main/java/DiceRoller.java
@@ -2,16 +2,12 @@ import java.util.Random;
 
 public class DiceRoller {
     public static void main(String[] args) {
-        // TODO: Create Random objects for dice rolling
-        // 1. Three Random objects without seeds (unseeded dice)
-        // 2. One Random object with any seed value (seeded die)
-        
-        // TODO: Generate random integers for dice rolls
-        
-        // TODO: Display the dice rolls - 1-6
-        // Show all four dice rolls with clear labels
-        
-        // TODO: Demonstrate the difference between seeded and unseeded
-        // Run the program multiple times to show seeded consistency
+        Random rand = new Random();
+        System.out.println("Unseeded Die 1: " + ((int)(Math.random() * 6 + 1)));
+        System.out.println("Unseeded Die 2: " + ((int)(Math.random() * 6 + 1)));
+        System.out.println("Unseeded Die 3: " + ((int)(Math.random() * 6 + 1)));
+
+        Random rand1 = new Random(923764);
+        System.out.println("Seeded Die: " + ((rand1.nextInt(6) + 1)));
     }
 }
